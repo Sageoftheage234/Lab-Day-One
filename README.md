8928330374065af2b7ae55ebd8ed444155e5f450
sageoftheage234 <jourdane234@yahoo.com>
2019-02-21 20:40:39 -0500

    Created html, css, and js files

---
+++ README.md
@@ -0,0 +1,6 @@
+# Demo on HTML , JavaScript, and CSS
+- Created Repository
+- Created git repo locally ; and cd'd to the local folder
+- Created HTML
+- Created CSS
+- Created JavaScript
---
+++ main.js
@@ -0,0 +1,9 @@
+let yourName = prompt('What is your name champ?')
+let yourFavoriteColor = prompt('What is your favorite color?')
+let yourFavoriteNumber = prompt('What is your favorite number?')
+let yourFavoriteVacation = prompt('What is your favorite vacation destination?')
+confirm(`Very nice, ${yourName}. So your favorite color is ${yourFavoriteColor} and your favorite number is ${yourFavoriteNumber} and your favorite vacaction spot is ${yourFavoriteVacation}?`)
+
+console.log(`${yourName} your favorite color is ${yourFavoriteColor} and your favorite number is ${yourFavoriteNumber} and your favorite vacation spot is ${yourFavoriteVacation}?`)
+
+
